42f7bd97c9b0c66625e90ccf134bb612ff81bbb8
อภิสรา ทองสังข์ <58841002+apisaraa@users.noreply.github.com>
2020-03-07 19:50:17 +0700

    c

---
+++ coffee/src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+    public static void main(String [] arge ){
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+        coffeeMachine.MakeCoffee(6);
+    }
+}
